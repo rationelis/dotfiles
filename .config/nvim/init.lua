efa32ef52ac0f940cdebb177e2c12a406e91ca71
Cornee van Blerk <corneevanblerk@gmail.com>
2025-06-19 21:50:03 +0200

    feat: add auto update lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,4 +23,10 @@ require("lazy").setup({
 	checker = { enabled = true },
 })
 
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		require("lazy").update({ show = false })
+	end,
+})
+
 require("config")
