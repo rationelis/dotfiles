2a6aa63cc5cb53586ca50f3682c8e7a6c353fbb5
Rationelis <corneevanblerk@gmail.com>
2025-06-22 11:17:52 +0200

    fix: better update cache

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
+~/.config/polybar/scripts/updates.sh
+
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars on all monitors
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    echo "Launching polybar on monitor: $m"
-    MONITOR=$m polybar --reload main &
-  done
+if command -v xrandr >/dev/null 2>&1; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        echo "Launching polybar on monitor: $m"
+        MONITOR=$m polybar --reload main &
+    done
 else
-  polybar --reload main &
+    polybar --reload main &
 fi
 
 echo "Polybar launched..."
--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Simple update checker
+updates_file="/tmp/polybar_updates_shared"
+
+echo "Updating package count..."
 count=$(checkupdates 2>/dev/null | wc -l)
-
 if command -v yay >/dev/null 2>&1; then
-    aur_count=$(yay -Qum 2>/dev/null | wc -l)
-    count=$((count + aur_count))
+    count=$((count + $(yay -Qum 2>/dev/null | wc -l)))
 fi
 
 if [[ $count -eq 0 ]]; then
-    echo " ✅"
+    echo " ✅" > $updates_file
 else
-    echo " $count updates available"
+    echo " $count updates available" > $updates_file
 fi
